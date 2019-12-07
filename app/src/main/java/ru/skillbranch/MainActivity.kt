5e0db5c0661b431f937b6229310b64ad3f2c6de3
neestell <neestell@gmail.com>
2019-12-07 17:52:00 +0600

    Add splash fragment

--- app/src/main/java/ru/skillbranch/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/MainActivity.kt
@@ -1,12 +1,26 @@
 package ru.skillbranch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) removeSplashWithDelay()
+    }
+
+    private fun removeSplashWithDelay() {
+        supportFragmentManager.fragments.firstOrNull()
+            ?.let { splash ->
+                window.decorView.postDelayed({
+                    supportFragmentManager.beginTransaction()
+                        .setCustomAnimations(0, android.R.anim.fade_out)
+                        .remove(splash)
+                        .commit()
+                }, 5000)
+            }
     }
 }
---
+++ app/src/main/java/ru/skillbranch/SplashFragment.kt
@@ -0,0 +1,15 @@
+package ru.skillbranch
+
+import android.content.res.Configuration
+import androidx.fragment.app.Fragment
+
+
+/**
+ * @author neestell on 2019-12-07.
+ */
+class SplashFragment : Fragment(R.layout.f_splash) {
+
+    companion object {
+        fun newInstance() = SplashFragment()
+    }
+}
