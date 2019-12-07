0713343c8728da93b207893f9f7d3b74c7c06209
neestell <neestell@gmail.com>
2019-12-07 17:59:36 +0600

    Add empty characters and character fragment

---
+++ app/src/main/java/ru/skillbranch/CharacterFragment.kt
@@ -0,0 +1,10 @@
+package ru.skillbranch
+
+import androidx.fragment.app.Fragment
+
+
+/**
+ * @author neestell on 2019-12-07.
+ */
+class CharacterFragment : Fragment(R.layout.f_character) {
+}
---
+++ app/src/main/java/ru/skillbranch/CharactersFragment.kt
@@ -0,0 +1,10 @@
+package ru.skillbranch
+
+import androidx.fragment.app.Fragment
+
+
+/**
+ * @author neestell on 2019-12-07.
+ */
+class CharactersFragment : Fragment(R.layout.f_characters) {
+}
